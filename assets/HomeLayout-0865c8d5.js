c123b305550912073415984342b36e146b116a01
mxliu5 <mxliu5@iflytek.com>
2024-12-27 15:07:00 +0800

    update

--- assets/HomeLayout-0865c8d5.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index-f30f1b4a.js";import{j as t,o as r,p as c,C as s}from"./vendor-47c5f7a7.js";const n={},a={class:"home-layout"};function _(m,i){const e=t("router-view");return r(),c("div",a,[s(e)])}const l=o(n,[["render",_]]);export{l as default};
